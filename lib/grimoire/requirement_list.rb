844c3bcdd0106e811de6770b977e993a3710f907
Chris Roberts <chrisroberts.code@gmail.com>
2015-03-12 13:05:37 -0700

    Fetch constants when building

--- lib/grimoire/requirement_list.rb
+++ lib/grimoire/requirement_list.rb
@@ -5,7 +5,7 @@ module Grimoire
   # Requirment list for solver
   class RequirementList < Utility
     attribute :name, String, :required => true, :coerce => lambda{|val| val.to_s}
-    attribute :requirements, DEPENDENCY_CLASS, :multiple => true, :default => [], :coerce => lambda{|val| DEPENDENCY_CLASS.new(val.first, *val.last)}
+    attribute :requirements, DEPENDENCY_CLASS, :multiple => true, :default => [], :coerce => lambda{|val| Grimoire.const_get(:DEPENDENCY_CLASS).new(val.first, *val.last)}
   end
 
 end
--- lib/grimoire/unit.rb
+++ lib/grimoire/unit.rb
@@ -6,8 +6,8 @@ module Grimoire
   class Unit < Utility
 
     attribute :name, String, :required => true
-    attribute :dependencies, DEPENDENCY_CLASS, :multiple => true, :default => [], :coerce => lambda{|val| DEPENDENCY_CLASS.new(val.first, *val.last)}
-    attribute :version, VERSION_CLASS, :required => true, :coerce => lambda{|val| VERSION_CLASS.new(val)}
+    attribute :dependencies, DEPENDENCY_CLASS, :multiple => true, :default => [], :coerce => lambda{|val| Grimoire.const_get(:DEPENDENCY_CLASS).new(val.first, *val.last)}
+    attribute :version, VERSION_CLASS, :required => true, :coerce => lambda{|val| Grimoire.const_get(:VERSION_CLASS).new(val)}
 
   end
 
