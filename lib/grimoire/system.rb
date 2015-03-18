534dda63386ea1deec4f99c56ec1086d834264cc
Chris Roberts <chrisroberts.code@gmail.com>
2015-03-18 15:02:46 -0700

    If requested unit name has no units loaded within system, kick back an error

--- lib/grimoire/system.rb
+++ lib/grimoire/system.rb
@@ -58,6 +58,9 @@ module Grimoire
       unless(constraint.respond_to?(:requirements))
         raise TypeError.new "Expecting `#{REQUIREMENT_CLASS}` but received `#{constraint.class}`"
       end
+      unless(units[unit_name])
+        raise KeyError.new("Failed to locate any units loaded in system with requested name: `#{unit_name}`")
+      end
       units[unit_name].find_all do |unit|
         constraint.satisfied_by?(unit.version)
       end
