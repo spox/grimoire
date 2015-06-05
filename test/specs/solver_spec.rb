f267f77a9f2414d2d401d1ea521a2da0338750dd
Chris Roberts <code@chrisroberts.org>
2015-06-05 06:17:26 -0700

    Add coverage for restriction usage within solver

--- test/specs/solver_spec.rb
+++ test/specs/solver_spec.rb
@@ -154,5 +154,76 @@ describe Grimoire::Solver do
       solv.world.units['unit6'].size.must_equal 1
     end
 
+    it 'should apply restrictions when solving requirements' do
+      req = Grimoire::RequirementList.new(
+        :name => 'test',
+        :requirements => [
+          ['unit1', '> 5'],
+          ['unit2', '> 3']
+        ]
+      )
+      restrict = Grimoire::RequirementList.new(
+        :name => 'restrictions',
+        :requirements => [
+          ['unit1', '< 9'],
+          ['unit1', '<= 7'],
+          ['unit2', '> 4', '< 6']
+        ]
+      )
+      solv = Grimoire::Solver.new(
+        :system => system,
+        :requirements => req,
+        :restrictions => restrict
+      )
+      result = solv.generate!
+      path = result.pop
+      path.units.detect do |unit|
+        unit.name == 'unit1' && unit.version.to_s == '7.0.0'
+      end.wont_be_nil
+      path.units.detect do |unit|
+        unit.name == 'unit2' && unit.version.to_s == '5.0.0'
+      end.wont_be_nil
+    end
+
+    it 'should apply restrictions on dependencies when solving requirements' do
+      req = Grimoire::RequirementList.new(
+        :name => 'test',
+        :requirements => [
+          ['unit1', '> 5'],
+          ['unit2', '> 3']
+        ]
+      )
+      restrict = Grimoire::RequirementList.new(
+        :name => 'restrictions',
+        :requirements => [
+          ['unit1', '< 9'],
+          ['unit1', '<= 7'],
+          ['unit2', '> 4', '< 6'],
+          ['unit6', '< 6']
+        ]
+      )
+      units.detect do |unit|
+        unit.name == 'unit1' && unit.version.to_s == '7.0.0'
+      end.dependencies = [
+        ['unit6', '> 4', '< 9']
+      ]
+      solv = Grimoire::Solver.new(
+        :system => system,
+        :requirements => req,
+        :restrictions => restrict
+      )
+      result = solv.generate!
+      path = result.pop
+      path.units.detect do |unit|
+        unit.name == 'unit1' && unit.version.to_s == '7.0.0'
+      end.wont_be_nil
+      path.units.detect do |unit|
+        unit.name == 'unit2' && unit.version.to_s == '5.0.0'
+      end.wont_be_nil
+      path.units.detect do |unit|
+        unit.name == 'unit6' && unit.version.to_s == '5.0.0'
+      end.wont_be_nil
+    end
+
   end
 end
